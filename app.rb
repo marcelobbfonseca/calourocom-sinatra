02442baa04ca09599e8ad45f30beee95f9e121a6
marcelobbfonseca <marcelobbff@gmail.com>
2019-01-18 16:59:59 -0200

    added CRUD routes for institutes and posts

--- app.rb
+++ app.rb
@@ -81,7 +81,87 @@ class Application < Sinatra::Base
     end
 
 
-    # after do
-    #     response.body = JSON.dump(response.body)
-    # end
+    #institute routes
+    get '/institutes' do
+        institutes = Institute.all
+        json institutes
+    end
+
+    get '/institutes/:institute_id' do
+        institute = Institute.find(params[:institute_id])
+        json institute
+    end
+
+    post '/institutes' do
+        institute_params = MultiJson.load(response.body.read)
+        institute = Institute.save(institute_params)
+        if institute.save
+            json institute
+        else
+            json institute.errors.messages
+        end
+    end
+
+    put '/institutes' do
+        institute_params = MultiJson.load(response.body.read)
+        if institute.update(institute_params)
+            response = {status: 200, data:'success, institute updated.'}
+            json response
+        else
+            json institute.errors.messages
+        end
+    end
+
+    delete '/insitutes/:institute_id' do
+        institute = Institute.find(params[:institute_id])
+        no_data! unless institute
+        institute.destroy
+        response = {status: 200, data:'success, institute deleted.'}
+        json response
+    end
+    #posts routes
+
+    get 'institutes/:institute_id/posts' do
+        institute = Institute.find(params[:institute_id])
+        no_data! unless institute
+        json institute.posts
+    end
+
+    get 'institutes/:institute_id/posts/:post_id' do
+        post = Post.find(params[:post_id])
+        no_data! unless post
+        json post
+    end
+
+    post 'institutes/:institute_id/posts' do
+        
+        post_params = MultiJson.load(response.body.read)
+        post = Post.new(post_params)
+        post.institute = Institute.find(params[:institute_id])
+        if post.save
+            json post
+        else 
+            json post.errors.messages
+        end
+    end
+
+    put 'posts/:post_id' do
+        post = Post.find(params[:post_id])
+        no_data! unless post
+        post_params = MultiJson.load response.body.read
+        if post.update(post_params)
+            response = { message: 'success. Post updated', status: 200}
+            json response
+        else
+            json user.errors.messages
+        end
+    end
+
+    delete '/posts/:post_id' do
+        post = Post.find(params[:post_id])
+        no_data! unless post
+        post.destroy
+        response = {status: 200, data:'success, post deleted.'}
+        json response
+    end
 end
